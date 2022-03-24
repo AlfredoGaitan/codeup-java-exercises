7c5e698df51c686f5bbc5d730e108d90965676ec
AlfredoGaitan <gaitan.alfredo78@gmail.com>
2022-03-24 16:34:42 -0500

    add syntax,types and variables exercise from main HelloWorld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,58 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+
+        int myFavoriteNumber = 10;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Hello";
+        System.out.println("myString = " + myString);
+
+        char newCharValue = 'c';
+       // myString = (char) newCharValue;
+
+        double testNum = 3.14159;
+        //myString = (double) testNum;
+
+        float myNumber = 3.14f;
+        System.out.println("myNumber = " + myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String class = "class";
+
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+
+        System.out.println(o);
+
+//        int x = 4;
+//        x = x + 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x = x / y;
+        y = y - x;
+        System.out.println(x);
+        System.out.println(y);
+
+
+
     }
+
+
 }
+
---
+++ src/YoHey.java
@@ -0,0 +1,9 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class YoHey {
+    public static void main(String[] args) {
+        System.out.println("yo hey!");
+    }
+}
+
+
