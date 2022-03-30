64c7b8fe10b0ad48543b251b74bf7a47364ce6e3
AlfredoGaitan <gaitan.alfredo78@gmail.com>
2022-03-30 10:31:28 -0500

    add string exercise solution

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+
+        //a. While loop
+        Scanner scanner = new Scanner(System.in);
+//        int i = 5;
+//        while(i <= 15) {
+//            System.out.print(i + " ");
+//            i++;
+//        }
+
+//        for(int i=5;i<=15;i++) {
+//            System.out.println(i + " ");
+//        }
+
+        //b. Do while
+//      int a = 0;
+//        do {
+//            System.out.print(a);
+//            a += 2;
+//        } while (a <= 100);
+//
+//        for(int t=0;t<=100;t+=2) {
+//            System.out.print(t + " ");
+//        }
+
+//        int f = 100;
+//        do {
+//            System.out.print(f + " ");
+//            f -= 5;
+//        } while (f >= -10);
+//
+//        for(int i=100;i>=-10;i-=5) {
+//            System.out.print(i + " ");
+//        }
+
+//        long g = 2;
+//        do {
+//            System.out.println(g);
+//            g *= g;
+//        } while (g < 1000000);
+//
+//        for(long i=2;i<1000000;i*=i) {
+//            System.out.println(i);
+//        }
+
+
+//        for(int i=1;i<=100;i++) {
+//            if(i % 15 == 0) {
+//                System.out.println("FizzBuzz");
+//            } else if (i % 5 == 0) {
+//                System.out.println("Buzz");
+//            } else if (i % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+
+        //fizzBuzz(100);
+
+        System.out.print("Enter an integer to go up to: ");
+        int userInput = Integer.parseInt(scanner.next());
+        System.out.println("Here is your table!\n");
+        System.out.println("Number | Squared | Cubed");
+        System.out.println("------ | ------- | -----");
+        for (int i=1; i<=userInput; i++) {
+            System.out.printf ("%-6d | %-7d | %d\n", i, i*i, i*i*i);
+        }
+
+
+
+
+
+
+    }
+}
---
+++ src/StringExercise.java
@@ -0,0 +1,14 @@
+public class StringExercise {
+    public static void main(String[] args) {
+
+        String output = "We don't need no education";
+        System.out.println(output);
+        System.out.println(output.replace("education", "thought control"));
+        output = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(output);
+        output = "In windows, the main drive is usually C:\\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(output);
+
+    }
+}
