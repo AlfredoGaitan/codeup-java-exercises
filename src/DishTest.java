d96e2fae8dc882a377f43c955d769d5977797a0c
AlfredoGaitan <gaitan.alfredo78@gmail.com>
2022-04-04 09:13:17 -0500

    add circle and circleapp exercise

--- src/DishTest.java
+++ src/DishTest.java
@@ -1,89 +1,89 @@
-class Dish {
-    private String nameOfDish;
-    private int costInCents;
-    private boolean wouldRecommend;
-    private static  int totalDishes =0;
-    private static int discountPercent = 0;
-
-    private Dish(){
-
-        totalDishes++;
-    }
-
-    public Dish(String nameOfDish, int costInCents, boolean wouldRecommend) {
-        this();
-        this.nameOfDish = nameOfDish;
-        this.costInCents = costInCents;
-        this.wouldRecommend = wouldRecommend;
-    }
-
-    public void setNameOfDish(String nameOfDish) {
-        this.nameOfDish = nameOfDish;
-    }
-
-    public void setCostInCents(int costInCents) {
-        this.costInCents = costInCents;
-    }
-
-    public void setWouldRecommend(boolean wouldRecommend) {
-        this.wouldRecommend = wouldRecommend;
-    }
-
-    public static void setTotalDishes(int totalDishes) {
-        Dish.totalDishes = totalDishes;
-    }
-
-    public String getNameOfDish() {
-        return nameOfDish;
-    }
-
-    public int getCostInCents() {
-        return costInCents;
-    }
-
-    public boolean isWouldRecommend() {
-        return wouldRecommend;
-    }
-
-    public static int getTotalDishes() {
-        return totalDishes;
-    }
-
-    public void printSummary() {
-
-        float new = costInCents * (1-(float)discountPercent(100));
-
-        System.out.printf(
-                "\nCost: %d \n Name: %s \n Recommend: %b", costInCents, nameOfDish, wouldRecommend);
-
-    }
-}
-
-public class DishTest{
-
-    public static void main(String[] args) {
-
-
-
-        Dish.setDiscount(15);
-
-        Dish dish1 = new Dish();//
-
-        dish1.setNameOfDish(); = "Dragon Roll";
-        dish1.setCostInCents(); = 15000;
-        dish1.setWouldRecommend(); = true;
-
-        dish1.printSummary();
-
-        Dish dish2 = new Dish();
-
-        dish2.printSummary();
-
-        System.out.println("Dish.totalDishes = " + Dish.getTotalDishes());
-    }
-
-
-}
+//class Dish {
+//    private String nameOfDish;
+//    private int costInCents;
+//    private boolean wouldRecommend;
+//    private static  int totalDishes =0;
+//    private static int discountPercent = 0;
+//
+//    private Dish(){
+//
+//        totalDishes++;
+//    }
+//
+//    public Dish(String nameOfDish, int costInCents, boolean wouldRecommend) {
+//        this();
+//        this.nameOfDish = nameOfDish;
+//        this.costInCents = costInCents;
+//        this.wouldRecommend = wouldRecommend;
+//    }
+//
+//    public void setNameOfDish(String nameOfDish) {
+//        this.nameOfDish = nameOfDish;
+//    }
+//
+//    public void setCostInCents(int costInCents) {
+//        this.costInCents = costInCents;
+//    }
+//
+//    public void setWouldRecommend(boolean wouldRecommend) {
+//        this.wouldRecommend = wouldRecommend;
+//    }
+//
+//    public static void setTotalDishes(int totalDishes) {
+//        Dish.totalDishes = totalDishes;
+//    }
+//
+//    public String getNameOfDish() {
+//        return nameOfDish;
+//    }
+//
+//    public int getCostInCents() {
+//        return costInCents;
+//    }
+//
+//    public boolean isWouldRecommend() {
+//        return wouldRecommend;
+//    }
+//
+//    public static int getTotalDishes() {
+//        return totalDishes;
+//    }
+//
+//    public void printSummary() {
+//
+//        float new = costInCents * (1-(float)discountPercent(100));
+//
+//        System.out.printf(
+//                "\nCost: %d \n Name: %s \n Recommend: %b", costInCents, nameOfDish, wouldRecommend);
+//
+//    }
+//}
+//
+//public class DishTest{
+//
+//    public static void main(String[] args) {
+//
+//
+//
+////        Dish.setDiscount(15);
+//
+//        Dish dish1 = new Dish();//
+//
+//        dish1.setNameOfDish(); = "Dragon Roll";
+//        dish1.setCostInCents(); = 15000;
+//        dish1.setWouldRecommend(); = true;
+//
+//        dish1.printSummary();
+//
+//        Dish dish2 = new Dish();
+//
+//        dish2.printSummary();
+//
+//        System.out.println("Dish.totalDishes = " + Dish.getTotalDishes());
+//    }
+//
+//
+//}
 
 
 
---
+++ src/shapes/CircleApp.java
@@ -0,0 +1,21 @@
+package shapes;
+
+import java.util.Scanner;
+import util.Input;
+
+
+public class CircleApp {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Input().getScanner();
+
+        System.out.print("Please enter the radius of your circle: ");
+        double entry = scan.nextDouble();
+
+        Circle newCircle = new Circle(entry);
+
+        System.out.println(newCircle.getArea());
+        System.out.println(newCircle.getCircumference());
+    }
+}
